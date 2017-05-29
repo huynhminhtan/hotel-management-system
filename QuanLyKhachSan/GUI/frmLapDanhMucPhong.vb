3528bfa8058a80d4c75f1f9cc2dce9e40bb08700
ZelTar <aldaris2012@gmail.com>
2017-05-29 21:30:41 +0700

    - Sửa GUI/frmLapDanhMucPhong : định dạng bảng, tạo event click "Thêm phòng", sinh Mã phòng. Chưa hoàn chỉnh!

--- QuanLyKhachSan/GUI/frmLapDanhMucPhong.vb
+++ QuanLyKhachSan/GUI/frmLapDanhMucPhong.vb
@@ -1,6 +1,10 @@
 ﻿Imports BUS.BUS
+Imports DTO.DTO
 
 Public Class frmLapDanhMucPhong
+
+    Private danhSachPhongTam As New DataTable
+
 
     Private Sub frmLapDanhMucPhong_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -23,11 +27,19 @@ Public Class frmLapDanhMucPhong
         cboTenLoaiPhong.DataSource = LoaiPhongBUS.selectLoaiPhongAll()
 
         ' hiển thị mã loại phòng đã tăng
-        'txtMaPhong.Text = tangMaPhong(PhongBUS.selectMaPhongMoiNhat() 'as String)
+        txtMaPhong.Text = tangMaphong(PhongBUS.selectMaPhongMoiNhat())
+
+        ' khởi tạo bảng phòng tạm
+        danhSachPhongTam.Columns.Add("MaPhong", GetType(String))
+        danhSachPhongTam.Columns.Add("TenPhong", GetType(String))
+        danhSachPhongTam.Columns.Add("MaLoaiPhong", GetType(String))
+        danhSachPhongTam.Columns.Add("TenLoaiPhong", GetType(String))
+        danhSachPhongTam.Columns.Add("DonGia", GetType(Double))
+        danhSachPhongTam.Columns.Add("GhiChu", GetType(String))
 
         ' hiển thị danh sách các loại phòng tạm
-        ' Dim danhSachPhongTam As New List(Of PhongDTO)
-
+        dgvDanhMucPhong.AutoGenerateColumns = False
+        dgvDanhMucPhong.DataSource = danhSachPhongTam
 
     End Sub
 
@@ -42,6 +54,19 @@ Public Class frmLapDanhMucPhong
 
     Private Sub btnThemPhong_Click(sender As Object, e As EventArgs) Handles btnThemPhong.Click
 
+        If (String.IsNullOrEmpty(txtTenPhong.Text)) Then
+            MessageBox.Show("Vui lòng nhập tên phòng.")
+            Return
+        End If
+
+        danhSachPhongTam.Rows.Add(txtMaPhong.Text, txtTenPhong.Text, cboTenLoaiPhong.SelectedItem.MaLoaiPhong,
+                                  cboTenLoaiPhong.SelectedItem.TenLoaiPhong, txtDonGia.Text, txtGhiChu.Text)
+
+        dgvDanhMucPhong.DataSource = Nothing
+        dgvDanhMucPhong.DataSource = danhSachPhongTam
+
+        ' tăng mã phòng
+        txtMaPhong.Text = tangMaphong(danhSachPhongTam.Rows(danhSachPhongTam.Rows.Count - 1).Item(0).ToString)
 
     End Sub
 
@@ -51,7 +76,34 @@ Public Class frmLapDanhMucPhong
             Return "PH000"
         End If
 
-        Return ""
+        Dim maPhongTang As String
+
+        maPhongTang = "PH" + ((Integer.Parse(maPhong.Substring(2)) + 1).ToString).PadLeft(3, "0")
+
+        Return maPhongTang
     End Function
 
+    Private Sub dgvDanhMucPhong_SelectionChanged(sender As Object, e As EventArgs) Handles dgvDanhMucPhong.SelectionChanged
+        If (dgvDanhMucPhong.CurrentRow IsNot Nothing) Then
+            txtMaPhong.Text = dgvDanhMucPhong.CurrentRow.Cells("MaPhong").Value.ToString
+            txtTenPhong.Text = dgvDanhMucPhong.CurrentRow.Cells("TenPhong").Value.ToString
+
+            'cboTenLoaiPhong.SelectedValue = dgvDanhMucPhong.CurrentRow.Cells("MaLoaiPhong").Value
+
+            txtDonGia.Text = dgvDanhMucPhong.CurrentRow.Cells("DonGia").Value.ToString
+            txtGhiChu.Text = dgvDanhMucPhong.CurrentRow.Cells("GhiChu").Value.ToString
+        End If
+    End Sub
+
+    'Private Sub dgvDanhMucPhong_CurrentCellChanged(sender As Object, e As EventArgs) Handles dgvDanhMucPhong.CurrentCellChanged
+    '    If (dgvDanhMucPhong.CurrentRow IsNot Nothing) Then
+    '        txtMaPhong.Text = dgvDanhMucPhong.CurrentRow.Cells("MaPhong").Value.ToString
+    '        txtTenPhong.Text = dgvDanhMucPhong.CurrentRow.Cells("TenPhong").Value.ToString
+
+    '        'cboTenLoaiPhong.SelectedValue = dgvDanhMucPhong.CurrentRow.Cells("MaLoaiPhong").Value
+
+    '        txtDonGia.Text = dgvDanhMucPhong.CurrentRow.Cells("DonGia").Value.ToString
+    '        txtGhiChu.Text = dgvDanhMucPhong.CurrentRow.Cells("GhiChu").Value.ToString
+    '    End If
+    'End Sub
 End Class
