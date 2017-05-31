ce0357acc8087ae0229f903a3a54c9102ff0e4ba
mtSiniChi <minhtan.itdev@gmail.com>
2017-05-31 10:04:22 +0700

    - sqlQuanLyKhachSan : Thêm hàm: selectThamSo - tạo lớp ThamSoDAO

---
+++ QuanLyKhachSan/DAO/ThamSoDAO.vb
@@ -0,0 +1,34 @@
+﻿Namespace DAO
+
+    Public Class ThamSoDAO
+
+#Region "ReTrieving"
+
+        Public Shared Function selectThamSoALL() As ThamSoDTO
+
+            Dim thamSo As New ThamSoDTO
+
+            Try
+                Dim dt As New DataTable
+                dt = SqlDataAccessHelper.ExecuteQuery("selectThamSoAll", Nothing)
+
+                If (dt.Rows.Count <= 0) Then
+                    Return Nothing ' không có dữ liệu nào trong CSDL
+                End If
+
+                Dim row As DataRow = dt.Rows(0)
+
+                thamSo.SoKhachToiDa = Integer.Parse(row("SoKhachToiDa").ToString)
+                thamSo.TiLePhuThu = Double.Parse(row("TiLePhuThu").ToString)
+
+            Catch ex As Exception
+                Throw ex
+            End Try
+            Return thamSo
+        End Function
+
+#End Region
+
+    End Class
+
+End Namespace
