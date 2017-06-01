6dae0a839e338d6eeee7d606d308fde52b3bf004
mtSiniChi <minhtan.itdev@gmail.com>
2017-06-01 15:13:48 +0700

    - tạo fromTraCuuPhong

---
+++ QuanLyKhachSan/GUI/frmTraCuuPhong.vb
@@ -0,0 +1,10 @@
+﻿Public Class frmTraCuuPhong
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblTenLoaiPhong.Click
+
+    End Sub
+End Class
