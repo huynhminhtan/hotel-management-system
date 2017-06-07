5308e4e794cb42c1e2c5b5327864ff1e3b3bbf6b
mtSiniChi <minhtan.itdev@gmail.com>
2017-06-07 17:33:41 +0700

    - upload file

---
+++ QuanLyKhachSan/GUI/QuanLyPhieuThue/frmQuanLyPhieuThue.vb
@@ -0,0 +1,27 @@
+﻿Imports GUI.XuLy
+
+Public Class frmQuanLyPhieuThue
+
+    Private Sub frmQuanLyPhieuThue_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        cboTinhTrangPhieuThue.SelectedIndex = 2
+
+    End Sub
+
+
+    Private Sub btnLapPhieuThuePhong_Click(sender As Object, e As EventArgs) Handles btnLapPhieuThuePhong.Click
+        Dim formLapPhieuThuePhong As New frmLapPhieuThuePhong(Me)
+        formLapPhieuThuePhong.Show()
+    End Sub
+
+    Public Sub CapNhat()
+        Using New CenteredMessageBox(Me)
+            MessageBox.Show("Đã update!")
+        End Using
+    End Sub
+
+    Private Sub btnLapHoaDon_Click(sender As Object, e As EventArgs) Handles btnLapHoaDon.Click
+        Dim formLapHoaDon As New frmLapHoaDon(Me)
+        formLapHoaDon.Show()
+    End Sub
+End Class
---
+++ QuanLyKhachSan/GUI/QuanLyPhong/frmQuanLyPhong.vb
@@ -0,0 +1,42 @@
+﻿Imports BUS.BUS
+Imports DTO.DTO
+Imports GUI.XuLy
+
+Public Class frmQuanLyPhong
+
+    Private bangQuanLyPhong As New DataTable
+
+    Private Sub frmQuanLyPhong_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' khởi tạo bangQuanLyPhong
+        bangQuanLyPhong.Columns.Add("MaPhong", GetType(String))
+        bangQuanLyPhong.Columns.Add("TenPhong", GetType(String))
+        bangQuanLyPhong.Columns.Add("TenLoaiPhong", GetType(String))
+        bangQuanLyPhong.Columns.Add("DonGiaThue", GetType(Double))
+        bangQuanLyPhong.Columns.Add("TinhTrang", GetType(String))
+
+        ' lấy dữ liệu cho bảng
+        Dim danhSachPhong As List(Of PhongDTO)
+        danhSachPhong = PhongBUS.selectPhongAll()
+
+        For Each phong As PhongDTO In danhSachPhong
+            Dim tenLoaiPhong As String
+        Next
+    End Sub
+
+    Private Sub btnThemPhong_Click(sender As Object, e As EventArgs) Handles btnThemPhong.Click
+        Dim formLapDanhMucPhong As New frmLapDanhMucPhong(Me)
+        formLapDanhMucPhong.Show()
+    End Sub
+
+    Public Sub CapNhat()
+        Using New CenteredMessageBox(Me)
+            MessageBox.Show("Đã update!")
+        End Using
+    End Sub
+
+    Private Sub btnTimPhong_Click(sender As Object, e As EventArgs) Handles btnTimPhong.Click
+        Dim formTraCuuPhong As New frmTraCuuPhong(Me)
+        formTraCuuPhong.Show()
+    End Sub
+End Class
