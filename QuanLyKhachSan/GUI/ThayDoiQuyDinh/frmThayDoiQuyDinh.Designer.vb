097d44ef056656d48d2efbf83e83effbb3b6e903
ZelTar <aldaris2012@gmail.com>
2017-06-08 16:37:35 +0700

    - Cập nhật frmThayDoiQuyDinh: thiết kế hiển thị

--- QuanLyKhachSan/GUI/ThayDoiQuyDinh/frmThayDoiQuyDinh.Designer.vb
+++ QuanLyKhachSan/GUI/ThayDoiQuyDinh/frmThayDoiQuyDinh.Designer.vb
@@ -22,17 +22,157 @@ Partial Class frmThayDoiQuyDinh
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TableLayoutPanel3 = New System.Windows.Forms.TableLayoutPanel()
+        Me.txtBoxTiLePhuThu = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtBoxSoKhachToiDa = New System.Windows.Forms.TextBox()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.TableLayoutPanel3.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 0, 1)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 4
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.5!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 55.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(1296, 725)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 3
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel2.Controls.Add(Me.GroupBox1, 1, 0)
+        Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(3, 129)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 1
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(1290, 392)
+        Me.TableLayoutPanel2.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.GroupBox1.Controls.Add(Me.TableLayoutPanel3)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(261, 3)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(10)
+        Me.GroupBox1.Size = New System.Drawing.Size(768, 386)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Quy Định Khách Sạn"
+        '
+        'TableLayoutPanel3
+        '
+        Me.TableLayoutPanel3.ColumnCount = 2
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.Controls.Add(Me.txtBoxTiLePhuThu, 1, 1)
+        Me.TableLayoutPanel3.Controls.Add(Me.Label1, 0, 0)
+        Me.TableLayoutPanel3.Controls.Add(Me.Label2, 0, 1)
+        Me.TableLayoutPanel3.Controls.Add(Me.txtBoxSoKhachToiDa, 1, 0)
+        Me.TableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel3.Location = New System.Drawing.Point(10, 52)
+        Me.TableLayoutPanel3.Margin = New System.Windows.Forms.Padding(10)
+        Me.TableLayoutPanel3.Name = "TableLayoutPanel3"
+        Me.TableLayoutPanel3.RowCount = 2
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.Size = New System.Drawing.Size(748, 324)
+        Me.TableLayoutPanel3.TabIndex = 0
+        '
+        'txtBoxTiLePhuThu
+        '
+        Me.txtBoxTiLePhuThu.Dock = System.Windows.Forms.DockStyle.Top
+        Me.txtBoxTiLePhuThu.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBoxTiLePhuThu.Location = New System.Drawing.Point(414, 177)
+        Me.txtBoxTiLePhuThu.Margin = New System.Windows.Forms.Padding(40, 15, 40, 15)
+        Me.txtBoxTiLePhuThu.Name = "txtBoxTiLePhuThu"
+        Me.txtBoxTiLePhuThu.Size = New System.Drawing.Size(294, 34)
+        Me.txtBoxTiLePhuThu.TabIndex = 3
+        Me.txtBoxTiLePhuThu.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(15, 15)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(344, 132)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Số Khách Tối Đa:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.BottomRight
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(15, 177)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(344, 132)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Tỉ Lệ Phụ Thu (%):"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'txtBoxSoKhachToiDa
+        '
+        Me.txtBoxSoKhachToiDa.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.txtBoxSoKhachToiDa.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBoxSoKhachToiDa.Location = New System.Drawing.Point(414, 113)
+        Me.txtBoxSoKhachToiDa.Margin = New System.Windows.Forms.Padding(40, 15, 40, 15)
+        Me.txtBoxSoKhachToiDa.Name = "txtBoxSoKhachToiDa"
+        Me.txtBoxSoKhachToiDa.Size = New System.Drawing.Size(294, 34)
+        Me.txtBoxSoKhachToiDa.TabIndex = 2
+        Me.txtBoxSoKhachToiDa.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
         'frmThayDoiQuyDinh
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1296, 725)
+        Me.Controls.Add(Me.TableLayoutPanel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "frmThayDoiQuyDinh"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "frmThayDoiQuyDinh"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.TableLayoutPanel3.ResumeLayout(False)
+        Me.TableLayoutPanel3.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents TableLayoutPanel2 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TableLayoutPanel3 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtBoxSoKhachToiDa As System.Windows.Forms.TextBox
+    Friend WithEvents txtBoxTiLePhuThu As System.Windows.Forms.TextBox
 End Class
--- QuanLyKhachSan/GUI/ThayDoiQuyDinh/frmThayDoiQuyDinh.vb
+++ QuanLyKhachSan/GUI/ThayDoiQuyDinh/frmThayDoiQuyDinh.vb
@@ -1,3 +1,18 @@
 ﻿Public Class frmThayDoiQuyDinh
 
+    Private Sub txtBoxSoKhachToiDa_TextChanged(sender As Object, e As EventArgs) Handles txtBoxSoKhachToiDa.TextChanged
+        For Each kiTu As Char In txtBoxSoKhachToiDa.Text
+            If (Not (Char.IsControl(kiTu) Or Char.IsDigit(kiTu))) Then
+                txtBoxSoKhachToiDa.Text = Nothing
+            End If
+        Next
+    End Sub
+
+    Private Sub txtBoxTiLePhuThu_TextChanged(sender As Object, e As EventArgs) Handles txtBoxTiLePhuThu.TextChanged
+        For Each kiTu As Char In txtBoxTiLePhuThu.Text
+            If (Not (Char.IsControl(kiTu) Or Char.IsDigit(kiTu))) Then
+                txtBoxTiLePhuThu.Text = Nothing
+            End If
+        Next
+    End Sub
 End Class
